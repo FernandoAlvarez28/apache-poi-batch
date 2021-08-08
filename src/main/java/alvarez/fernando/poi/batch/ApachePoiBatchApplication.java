60e130be6e6f583fe599374392d57c2a31ed4efd
Fernando Alvarez <fernando44ca@gmail.com>
2021-08-08 16:02:08 -0300

    Initial files by Spring Initializr

---
+++ src/main/java/alvarez/fernando/poi/batch/ApachePoiBatchApplication.java
@@ -0,0 +1,13 @@
+package alvarez.fernando.poi.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApachePoiBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApachePoiBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alvarez/fernando/poi/batch/ApachePoiBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package alvarez.fernando.poi.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApachePoiBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
